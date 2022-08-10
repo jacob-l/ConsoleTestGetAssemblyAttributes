a5544918c9a36025a0f56a5970e2bf76527bcbc5
Iakov Lilo <jlilo@hwdtech.com>
2022-08-10 20:28:15 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Reflection;
+using System.Text;
+
+var sb = new StringBuilder();
+sb.AppendLine("Assembly:");
+var assembly = Assembly.GetEntryAssembly();
+
+var attributes = Attribute.GetCustomAttributes(assembly, typeof(AssemblyMetadataAttribute))
+    .OfType<AssemblyMetadataAttribute>().ToList();
+
+sb.AppendLine(assembly.FullName + ":");
+
+foreach (var att in attributes)
+{
+    sb.AppendLine(att.Key + " - " + att.Value);
+}
+
+Console.WriteLine("------");
+Console.WriteLine(sb);
