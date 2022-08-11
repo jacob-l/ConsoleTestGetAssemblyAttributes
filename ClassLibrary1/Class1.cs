14dffe9b7a3bdadd11be6bd8d43eb9cfcf970334
Iakov Lilo <jlilo@hwdtech.com>
2022-08-11 12:12:19 +0600

    Add class library

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Assembly:");
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var attributes = Attribute.GetCustomAttributes(assembly, typeof(AssemblyMetadataAttribute))
+                .OfType<AssemblyMetadataAttribute>().ToList();
+
+            sb.AppendLine(assembly.FullName + ":");
+
+            foreach (var att in attributes)
+            {
+                sb.AppendLine(att.Key + " - " + att.Value);
+            }
+
+            Console.WriteLine("------");
+            Console.WriteLine(sb);
+        }
+    }
+}
